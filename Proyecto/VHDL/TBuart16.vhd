4b9de9497fc36c6312329c11ab1ceeed29a40d3c
marcosChoucino <103415487+marcosChoucino@users.noreply.github.com>
2023-12-12 10:46:25 +0100

    Update TBuart16.vhd

--- Proyecto/VHDL/TBuart16.vhd
+++ Proyecto/VHDL/TBuart16.vhd
@@ -10,7 +10,7 @@ entity UART16TB  is
 end; 
  
 architecture a of UART16TB is
-  component UART16
+  component UART_16
 	port
 	(	--entradas
     reset,CLK		: in std_logic;
@@ -19,16 +19,8 @@ architecture a of UART16TB is
 
     --salidas
     MANDANDO		: out std_logic;
-    DATOS			: out std_logic_vector(15 downto 0);
+    DATOS			: out std_logic_vector(15 downto 0)
 
-
-	--salidas a borrar despues
-	DESPLAZAR2 : out std_logic;
-	TC_DIFF2 : out std_logic;
-	DIFF3 : out std_logic_vector(9 downto 0);
-	A_CARGAR2 : out std_logic_vector(9 downto 0);
-	LD_DIFF2 : out std_logic;
-	PASO2 : out std_logic_vector(4 downto 0)
 
 	); 
   end component ; 
@@ -45,15 +37,6 @@ architecture a of UART16TB is
 
 	
 
-	--Borrare despues
-	signal tb_PASO2		: std_logic_vector(4 downto 0);
-	signal tb_DESPLAZAR2		: std_logic;
-	
-	signal tb_DIFF3		: std_logic_vector(9 downto 0);
-	signal tb_LD_DIFF2	 : std_logic;
-	signal tb_TC_DIFF2	: std_logic;
-
-	signal tb_ACARGAR2		: std_logic_vector(9 downto 0);
 
 
 
@@ -63,7 +46,7 @@ architecture a of UART16TB is
 
 
 begin
-	DUT: UART16
+	DUT: UART_16
 	port map ( 
 -- mapeando 
 -- mi codigo es:
@@ -75,21 +58,16 @@ RX		 	=> tb_RX,
 
 MANDANDO => tb_MANDANDO,
 
-DATOS			=> tb_DATOS,
-DIFF3=> tb_DIFF3,
+DATOS => tb_DATOS
+
 
 --BORRARE DESPUES
-A_CARGAR2 => tb_ACARGAR2,
-PASO2 => tb_PASO2,
-DESPLAZAR2 => tb_DESPLAZAR2,
 
-TC_DIFF2 => tb_TC_DIFF2,
 
-LD_DIFF2 => tb_LD_DIFF2
 
 
 	);
---peque�a explicacion de lo que voy ha hacer:
+--peque?a explicacion de lo que voy ha hacer:
 -- la uart manda 115200 bits cada 1 segundo, 
 --tras hacer la inversa me queda q manda un bit cada
 --86805 nanosegundos
@@ -111,7 +89,7 @@ process
 	--empiezo a hacer cosas	
 
 	tb_RX<= '0';
-	wait for 8680 ns; --el dato a leer es el numuero 10001000 11101110, para mandarlo tengo que pasarle la señal   0100010001011101110 (el primer 0 ya esta)
+	wait for 8680 ns; --el dato a leer es el numuero 10001000 11101110, para mandarlo tengo que pasarle la se�al   0100010001011101110 (el primer 0 ya esta)
 	--primer dato
 	tb_RX<= '1';--primer dato es un 1	
 	wait for 8680 ns; 
@@ -165,7 +143,7 @@ wait for 500 ns; --un poco mas tarde mando recibido
 tb_RECIBIDO <= '1';
 
 
---y mando otro dato, esta vez quiero mandar 10011001   01100110, para eso le añadire el 0 inicial, un 1 y un 0 a la mitad, y el 1 al final: 010011001100010011
+--y mando otro dato, esta vez quiero mandar 10011001   01100110, para eso le a�adire el 0 inicial, un 1 y un 0 a la mitad, y el 1 al final: 010011001100010011
 
 wait for 8680 ns;
 tb_RECIBIDO <= '0';
@@ -213,4 +191,3 @@ tb_RX<= '0';
     end process;
 
 end;
-
